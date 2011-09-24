1f0171459631f12efe118079489b54f208145c7e
Ilya Biryukov <ilya@curiousdeveloper.net>
2011-09-24 16:12:16 +0100

    First checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ce003b717a718b189bbb5444c48da76ac83ed5b47918de91e21d60c96cbf3f5593a1d8f8ace9a38fd6692148f5052adb25e513e9c743dd8d83639bf62b7d6bb7'
